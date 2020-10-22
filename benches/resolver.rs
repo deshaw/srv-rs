452a1875712b7562c91d4b581fbf52bf15203c70
Max Heller <hellerm@deshaw.com>
2020-10-22 14:32:41 -0400

    add trust-dns benches

--- benches/resolver.rs
+++ benches/resolver.rs
@@ -1,34 +1,62 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use srv_rs::resolver::{libresolv::LibResolv, SrvResolver};
+use trust_dns_resolver::{AsyncResolver, TokioAsyncResolver};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut runtime = tokio::runtime::Runtime::new().unwrap();
+    let libresolv = LibResolv::default();
+    let trust_dns = runtime
+        .block_on(AsyncResolver::tokio_from_system_conf())
+        .unwrap();
 
-    c.bench_function(
-        &format!("resolve {} (libresolv)", srv_rs::EXAMPLE_SRV),
-        |b| {
-            b.iter(|| {
-                runtime
-                    .block_on(LibResolv::default().get_srv_records_unordered(srv_rs::EXAMPLE_SRV))
-                    .unwrap()
-            })
-        },
-    );
-
-    c.bench_function("resolve _imaps._tcp.gmail.com (libresolv)", |b| {
+    let mut group = c.benchmark_group(format!("resolve {}", srv_rs::EXAMPLE_SRV));
+    group.bench_function("libresolv", |b| {
         b.iter(|| {
             runtime
-                .block_on(LibResolv::default().get_srv_records_unordered("_imaps._tcp.gmail.com"))
+                .block_on(libresolv.get_srv_records_unordered(srv_rs::EXAMPLE_SRV))
                 .unwrap()
         })
     });
+    group.bench_function("trust-dns", |b| {
+        b.iter(|| {
+            runtime
+                .block_on(trust_dns.get_srv_records_unordered(srv_rs::EXAMPLE_SRV))
+                .unwrap()
+        })
+    });
+    drop(group);
 
-    let (records, _) = runtime
-        .block_on(LibResolv::default().get_srv_records_unordered(srv_rs::EXAMPLE_SRV))
-        .unwrap();
+    let gmail = "_imaps._tcp.gmail.com";
+    let mut group = c.benchmark_group(format!("resolve {}", gmail));
+    group.bench_function("libresolv", |b| {
+        b.iter(|| {
+            runtime
+                .block_on(libresolv.get_srv_records_unordered(gmail))
+                .unwrap()
+        })
+    });
+    group.bench_function("trust-dns", |b| {
+        b.iter(|| {
+            runtime
+                .block_on(trust_dns.get_srv_records_unordered(gmail))
+                .unwrap()
+        })
+    });
+    drop(group);
+
+    let mut group = c.benchmark_group(format!("order {} records", srv_rs::EXAMPLE_SRV));
     let mut rng = rand::thread_rng();
-    c.bench_function(&format!("order {} records", srv_rs::EXAMPLE_SRV), |b| {
+    let (records, _) = runtime
+        .block_on(libresolv.get_srv_records_unordered(srv_rs::EXAMPLE_SRV))
+        .unwrap();
+    group.bench_function("libresolv", |b| {
         b.iter(|| LibResolv::order_srv_records(&mut records.clone(), &mut rng))
+    });
+    let (records, _) = runtime
+        .block_on(trust_dns.get_srv_records_unordered(srv_rs::EXAMPLE_SRV))
+        .unwrap();
+    group.bench_function("trust-dns", |b| {
+        b.iter(|| TokioAsyncResolver::order_srv_records(&mut records.clone(), &mut rng))
     });
 }
 
