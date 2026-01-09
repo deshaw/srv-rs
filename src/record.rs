42a01bba5cdab066cbb872835c60a98e14cddfa4
lvkv <velikovlukas@gmail.com>
2026-01-09 17:47:28 -0500

    build(deps): update rand from 0.7 to 0.9.2

--- src/record.rs
+++ src/record.rs
@@ -68,6 +68,6 @@ pub trait SrvRecord {
 /// Generates a key to sort a SRV record by priority and weight per RFC 2782.
 pub(crate) fn sort_key(priority: u16, weight: u16, mut rng: impl Rng) -> (u16, Reverse<u32>) {
     // Sort ascending by priority, then descending (hence `Reverse`) by randomized weight
-    let rand = rng.gen::<u16>() as u32;
+    let rand = rng.random::<u16>() as u32;
     (priority, Reverse(weight as u32 * rand))
 }
