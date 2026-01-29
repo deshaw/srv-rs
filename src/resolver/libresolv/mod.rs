af4ca90ed80157e5cef7a5c2d46e8af725880af4
Lukas Velikov <velikovlukas@gmail.com>
2026-01-29 17:56:26 -0500

    refactor(resolver): replace libresolv-sys with resolv
    This crate currently uses the `libresolv-sys` crate to implement its libresolv-backed resolver feature. The downsides are:
    - The version used, 0.2, is ancient (~2020), which has led us to drift out of date with glibc/libresolv on newer systems
    - It's a lower-level -sys crate, so the code is close to the FFI boundary and somewhat gnarly
    
    Thankfully, the author of the `libresolv-sys` crate has since released and updated a higher-level `resolv` crate that is (IMO) a much better fit for this project! Check out the negative diff.
    
    I'm also using this PR as an opportunity to sneak in a GitHub actions workflow. This isn't the final form of the workflow—future PRs will include Clippy lints, doc tests, and more.
    
    
    ---------
    
    Co-authored-by: Nicolas Dudebout <nicolas.dudebout@gmail.com>

--- src/resolver/libresolv/mod.rs
+++ src/resolver/libresolv/mod.rs
@@ -3,49 +3,45 @@
 use super::SrvResolver;
 use crate::SrvRecord;
 use async_trait::async_trait;
+use resolv::{Record, Resolver};
 use std::{
-    convert::TryInto,
-    ffi::CString,
+    cell::RefCell,
     time::{Duration, Instant},
 };
 
-mod ffi;
+// Per-thread libresolv resolver, where each resolver has a stable address.
+// This is required because:
+// - libresolv's res_state contains raw pointers, some of which are self-referential.
+// - The intended usage is to manage per-thread resolver state.
+//
+// From the man page for resolver(3):
+//
+//     The traditional resolver interfaces such as res_init() and res_query()
+//     use some static (global) state stored in the _res structure, rendering
+//     these functions non-thread-safe.
+//
+//     BIND 8.2 introduced a set of new interfaces res_ninit(), res_nquery(),
+//     and so on, which take a res_state as their first argument, so you can use
+//     a per-thread resolver state.
+//
+thread_local!(static RESOLVER: RefCell<Resolver> =
+    RefCell::new(Resolver::new().expect("unable to initialize libresolv state"))
+);
 
 /// Errors encountered by [`LibResolv`].
 #[derive(Debug, thiserror::Error, PartialEq, Eq)]
 pub enum LibResolvError {
-    /// Rust -> C string conversion errors.
-    #[error("srv name contained interior null byte: {0}")]
-    InteriorNul(#[from] std::ffi::NulError),
     /// SRV resolver errors.
     #[error("resolver: {0}")]
-    Resolver(#[from] ffi::ResolverError),
+    Resolver(#[from] resolv::error::Error),
     /// Tried to parse non-SRV record as SRV.
     #[error("record type is not SRV")]
     NotSrv,
-    /// DNS answer larger than allowed by RFC.
-    #[error("DNS answer did not fit in maximum message size (65535)")]
-    AnswerTooLarge,
 }
 
 /// SRV Resolver backed by `libresolv`.
-#[derive(Debug)]
-pub struct LibResolv {
-    initial_buf_size: usize,
-}
-
-impl LibResolv {
-    /// Initialzes a resolver with a specific initial buffer size for DNS answers.
-    pub fn new(initial_buf_size: usize) -> Self {
-        Self { initial_buf_size }
-    }
-}
-
-impl Default for LibResolv {
-    fn default() -> Self {
-        Self::new(ffi::NS_PACKETSZ as usize)
-    }
-}
+#[derive(Debug, Default)]
+pub struct LibResolv;
 
 #[async_trait]
 impl SrvResolver for LibResolv {
@@ -56,54 +52,17 @@ impl SrvResolver for LibResolv {
         &self,
         srv: &str,
     ) -> Result<(Vec<Self::Record>, Instant), Self::Error> {
-        let srv = CString::new(srv)?;
-        let mut buf = vec![0u8; self.initial_buf_size];
-        ffi::RESOLV_STATE.with(|state| {
-            let mut state = state.borrow_mut();
-            let (len, response_time) = loop {
-                let len = unsafe {
-                    ffi::res_nsearch(
-                        state.as_mut(),
-                        srv.as_ptr(),
-                        ffi::ns_c_in as i32,
-                        ffi::ns_t_srv as i32,
-                        buf.as_mut_ptr(),
-                        buf.len() as i32,
-                    )
-                };
-                let len = match state.check(len) {
-                    Ok(()) => len as usize,
-                    Err(e) => return Err(e.into()),
-                };
-                if len <= buf.len() {
-                    break (len, Instant::now());
-                } else if len <= ffi::NS_MAXMSG as usize {
-                    // Retry with larger buffer
-                    buf.resize(len, 0)
-                } else {
-                    return Err(LibResolvError::AnswerTooLarge);
-                }
-            };
-
-            let response = &buf[..len];
-            let mut msg = unsafe { std::mem::zeroed() };
-            let ret =
-                unsafe { ffi::ns_initparse(response.as_ptr(), response.len() as i32, &mut msg) };
-            state.check(ret)?;
-
-            let mut rr = unsafe { std::mem::zeroed() };
-            let num_records = ffi::ns_msg_count(msg, ffi::ns_s_an);
-            let mut records = Vec::with_capacity(num_records as usize);
-            let mut min_ttl = None;
-            for idx in 0..num_records {
-                let ret = unsafe { ffi::ns_parserr(&mut msg, ffi::ns_s_an, idx as i32, &mut rr) };
-                state.check(ret)?;
-                let (record, ttl) = LibResolvSrvRecord::try_parse(&state, msg, rr)?;
-                records.push(record);
-                min_ttl = min_ttl.min(Some(ttl)).or(Some(ttl));
-            }
-
-            Ok((records, response_time + min_ttl.unwrap_or_default()))
+        RESOLVER.with(|resolver| {
+            let mut resolver = resolver.borrow_mut();
+            let mut response: resolv::Response =
+                resolver.search(srv.as_bytes(), resolv::Class::IN, resolv::RecordType::SRV)?;
+            let response_time = Instant::now();
+            let (ttls, srvs): (Vec<Duration>, Vec<Self::Record>) = response
+                .answers::<resolv::record::SRV>()
+                .map(|x| (Duration::from_secs(u64::from(x.ttl)), Self::Record::from(x)))
+                .unzip();
+            let min_ttl = ttls.into_iter().min().unwrap_or(Duration::ZERO);
+            Ok((srvs, response_time + min_ttl))
         })
     }
 }
@@ -141,58 +100,25 @@ impl SrvRecord for LibResolvSrvRecord {
     }
 }
 
-impl LibResolvSrvRecord {
-    fn try_parse(
-        state: &ffi::ResolverState,
-        msg: ffi::ns_msg,
-        rr: ffi::ns_rr,
-    ) -> Result<(Self, Duration), LibResolvError> {
-        if rr.type_ as u32 != ffi::ns_t_srv {
-            return Err(LibResolvError::NotSrv);
+impl From<Record<resolv::record::SRV>> for LibResolvSrvRecord {
+    fn from(record: Record<resolv::record::SRV>) -> Self {
+        Self {
+            target: record.data.name,
+            port: record.data.port,
+            priority: record.data.priority,
+            weight: record.data.weight,
         }
-
-        let (header, rest) =
-            unsafe { std::slice::from_raw_parts(rr.rdata, rr.rdlength as usize) }.split_at(6);
-
-        let mut chunks = header
-            .chunks_exact(2)
-            .map(|chunk| u16::from_be_bytes(chunk.try_into().unwrap()));
-
-        let priority = chunks.next().unwrap();
-        let weight = chunks.next().unwrap();
-        let port = chunks.next().unwrap();
-
-        let mut name = [0u8; ffi::NS_MAXDNAME as usize];
-        let ret = unsafe {
-            ffi::dn_expand(
-                ffi::ns_msg_base(msg),
-                ffi::ns_msg_end(msg),
-                rest.as_ptr(),
-                name.as_mut_ptr().cast(),
-                name.len() as i32,
-            )
-        };
-        state.check(ret)?;
-
-        let target = unsafe { std::ffi::CStr::from_ptr(name.as_ptr().cast()) };
-        let ttl = Duration::from_secs(rr.ttl as u64);
-        let record = Self {
-            target: target.to_string_lossy().to_string(),
-            port,
-            priority,
-            weight,
-        };
-        Ok((record, ttl))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use resolv::error::{Error as ResolvError, ResolutionError};
 
     #[tokio::test]
     async fn srv_lookup() -> Result<(), LibResolvError> {
-        let (records, valid_until) = LibResolv::default()
+        let (records, valid_until) = LibResolv
             .get_srv_records_unordered(crate::EXAMPLE_SRV)
             .await?;
         assert_ne!(records.len(), 0);
@@ -202,9 +128,7 @@ mod tests {
 
     #[tokio::test]
     async fn srv_lookup_ordered() -> Result<(), LibResolvError> {
-        let (records, _) = LibResolv::default()
-            .get_srv_records(crate::EXAMPLE_SRV)
-            .await?;
+        let (records, _) = LibResolv.get_srv_records(crate::EXAMPLE_SRV).await?;
         assert_ne!(records.len(), 0);
         assert!((0..records.len() - 1).all(|i| records[i].priority() <= records[i + 1].priority()));
         Ok(())
@@ -212,41 +136,45 @@ mod tests {
 
     #[tokio::test]
     async fn invalid_host() {
+        let result = LibResolv
+            .get_srv_records("_http._tcp.foobar.deshaw.com")
+            .await;
         assert_eq!(
-            LibResolv::default()
-                .get_srv_records("_http._tcp.foobar.deshaw.com")
-                .await,
-            Err(ffi::ResolverError::HostNotFound.into())
+            result,
+            Err(LibResolvError::Resolver(ResolvError::Resolver(
+                ResolutionError::HostNotFound
+            )))
         );
     }
 
     #[tokio::test]
     async fn malformed_srv_name() {
+        let result = LibResolv.get_srv_records("_http.foobar.deshaw.com").await;
         assert_eq!(
-            LibResolv::default()
-                .get_srv_records("_http.foobar.deshaw.com")
-                .await,
-            Err(ffi::ResolverError::HostNotFound.into())
+            result,
+            Err(LibResolvError::Resolver(ResolvError::Resolver(
+                ResolutionError::HostNotFound
+            )))
         );
     }
 
     #[tokio::test]
     async fn very_malformed_srv_name() {
+        let result = LibResolv.get_srv_records("  @#*^[_hsd flt.com").await;
         assert_eq!(
-            LibResolv::default()
-                .get_srv_records("  @#*^[_hsd flt.com")
-                .await,
-            Err(ffi::ResolverError::HostNotFound.into())
+            result,
+            Err(LibResolvError::Resolver(ResolvError::Resolver(
+                ResolutionError::HostNotFound
+            )))
         );
     }
 
     #[tokio::test]
     async fn srv_name_containing_nul_terminator() {
+        let result = LibResolv.get_srv_records("_http.\0_tcp.foo.com").await;
         assert!(matches!(
-            LibResolv::default()
-                .get_srv_records("_http.\0_tcp.foo.com")
-                .await,
-            Err(LibResolvError::InteriorNul(_))
+            result,
+            Err(LibResolvError::Resolver(ResolvError::CString(_)))
         ));
     }
 }
